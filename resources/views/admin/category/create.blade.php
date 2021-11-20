d0d66a7d43bafbd18ae196f8550c3d9a89fc6f48
GodwillFoka <fokagodwill@gmail.com>
2021-11-21 00:21:53 +0100

    Reception de main et fusion avec mon travail

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -28,6 +28,42 @@
 
                     </form>
 
+                    <div class="modal fade bs-example-modal-lg" id="bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" style="display: none;" aria-hidden="true">
+                        <div class="modal-dialog modal-lg modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="modal-title" id="myLargeModalLabel">Ajouter un type catégori </h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                </div>
+                                <div class="modal-body">
+                                    <form method="POST" action="{{ url('/admin/type-category') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
+                                        {{ csrf_field() }}
+                                        <div class="form-group {{ $errors->has('nom') ? 'has-error' : ''}}">
+                                            <label for="nom" class="control-label">{{ 'Nom' }} <span class="text-red">*</span></label>
+                                            <input class="form-control" name="nom" type="text" id="nom"
+                                                value="{{ isset($type_categorie->nom) ? $type_categorie->nom : ''}}">
+                                            {!! $errors->first('nom', '<p class="help-block">:message</p>') !!}
+                                        </div>
+                                        <div class="form-group {{ $errors->has('description') ? 'has-error' : ''}}">
+                                            <label for="description" class="control-label">{{ 'Description' }} <span class="text-red">*</span></label>
+                                            <textarea class="form-control" rows="5" name="description" type="textarea"
+                                                id="description">{{ isset($type_categorie->description) ? $type_categorie->description : ''}}</textarea>
+                                            {!! $errors->first('description', '<p class="help-block">:message</p>') !!}
+                                        </div>
+                                        <div class="modal-footer">
+                                            <div class="form-group">
+                                                <input class="btn btn-primary" type="submit" value="Create">
+                                            </div>
+                                        </div>
+
+
+
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/admin/type-category/form.blade.php
+++ resources/views/admin/type-category/form.blade.php
@@ -1,11 +1,11 @@
 <div class="form-group {{ $errors->has('nom') ? 'has-error' : ''}}">
-    <label for="nom" class="control-label">{{ 'Nom' }}</label>
+    <label for="nom" class="control-label">{{ 'Nom' }} <span class="text-red">*</span></label>
     <input class="form-control" name="nom" type="text" id="nom"
         value="{{ isset($type_categorie->nom) ? $type_categorie->nom : ''}}">
     {!! $errors->first('nom', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('description') ? 'has-error' : ''}}">
-    <label for="description" class="control-label">{{ 'Description' }}</label>
+    <label for="description" class="control-label">{{ 'Description' }} <span class="text-red">*</span></label>
     <textarea class="form-control" rows="5" name="description" type="textarea"
         id="description">{{ isset($type_categorie->description) ? $type_categorie->description : ''}}</textarea>
     {!! $errors->first('description', '<p class="help-block">:message</p>') !!}
