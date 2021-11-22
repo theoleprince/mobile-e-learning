5ff1e931097e9639c8162a916d6aa95f8da53845
GodwillFoka <fokagodwill@gmail.com>
2021-11-22 07:49:25 +0100

    Gestion du forum

--- resources/views/admin/reponse-c/forum.blade.php
+++ resources/views/admin/reponse-c/forum.blade.php
@@ -33,25 +33,7 @@
                                 <div>
                                     <i class="fas fa-envelope bg-blue"></i>
                                     <div class="timeline-item">
-                                        {{-- @if ({{ $item->phase_id }} = 0)
-                                            <span class="time"><i class="fas fa-clock"></i> 12:05</span>
-                                            <h3 class="timeline-header"><a href="#"><a href="mailto:{{ $item->email }}" style="color: blue;">{{ $item->email }}</a></a> Visitor</h3>
-                                            <div class="timeline-body">
-                                                {{$item->commentaire}}
-                                            </div>
-                                            <div class="timeline-footer">
-                                                <a class="btn btn-primary btn-sm" href="{{ url('admin/forum' . '/' . $item->id) }}"><i class="fas fa-trash"
-                                                    aria-hidden="true"></i> Repondre</a>
-                                                <form method="POST" action="{{ url('admin/forum' . '/' . $item->id) }}"
-                                                    accept-charset="UTF-8" style="display:inline">
-                                                    {{ method_field('DELETE') }}
-                                                    {{ csrf_field() }}
-                                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete type category"
-                                                        onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fas fa-trash"
-                                                            aria-hidden="true"></i></button>
-                                                </form>
-                                            </div>
-                                        @else --}}
+                                        @if ( {{ $item->phase_id }} != 0)
                                             <span class="time"><i class="fas fa-clock"></i> 12:05</span>
                                             <h3 class="timeline-header"><a href="#"><a href="mailto:{{ $item->email }}" style="color: blue;">{{ $item->email }}</a></a> {{ $item->name }} {{ $item->prenom }}</h3>
                                             <div class="timeline-body">
@@ -69,6 +51,25 @@
                                                             aria-hidden="true"></i></button>
                                                 </form>
                                             </div>
+                                        @else
+                                            <span class="time"><i class="fas fa-clock"></i> 12:05</span>
+                                            <h3 class="timeline-header"><a href="#"><a href="mailto:{{ $item->email }}" style="color: blue;">{{ $item->email }}</a></a> Visitor</h3>
+                                            <div class="timeline-body">
+                                                {{$item->commentaire}}
+                                            </div>
+                                            <div class="timeline-footer">
+                                                <a class="btn btn-primary btn-sm" href="{{ url('admin/forum' . '/' . $item->id) }}"><i class="fas fa-trash"
+                                                    aria-hidden="true"></i> Repondre</a>
+                                                <form method="POST" action="{{ url('admin/forum' . '/' . $item->id) }}"
+                                                    accept-charset="UTF-8" style="display:inline">
+                                                    {{ method_field('DELETE') }}
+                                                    {{ csrf_field() }}
+                                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete type category"
+                                                        onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fas fa-trash"
+                                                            aria-hidden="true"></i></button>
+                                                </form>
+                                            </div>
+                                        @endif
                                     </div>
                                 </div>
                             @empty
